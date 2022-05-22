86815eda557b2973fd2f1000ede7cf8c2aac3947
myfreeguru <40752533+Akaramsaif@users.noreply.github.com>
2022-05-22 09:07:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>RR will win the Ipl. </p></body></html>');
+        res.write('<html><body><p>IP final match will be played on 29th May </p></body></html>');
         res.end();
 
     }
