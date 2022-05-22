b3950b06dbf52ce6747ef6779c580c4992536216
myfreeguru <40752533+Akaramsaif@users.noreply.github.com>
2022-05-22 08:42:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>Delhi has lost the match. </p></body></html>');
+        res.write('<html><body><p>RR will win the Ipl. </p></body></html>');
         res.end();
 
     }
