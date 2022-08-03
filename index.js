d5d517341443fc6ef869028c36cef7d20961f76c
myfreeguru <40752533+Akaramsaif@users.noreply.github.com>
2022-08-03 12:15:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is the default page. if you dont specify any endpoint/address this default page will be shown.inr1 </p></body></html>');
+        res.write('<html><body><p>This is the default page. if you dont specify any endpoint/address this default page will be shown. </p></body></html>');
         res.end();
 
     }
